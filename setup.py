b2ef9737b7a9f53fb90d8aeecaa30b105d1e232c
AndreSeku <andreseku@gmail.com>
2021-07-09 16:33:01 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'hctsa',
     packages = ['hctsa'],
-    version = 'v0.0.2',  # Ideally should be same as your GitHub release tag version
+    version = 'v0.0.3',  # Ideally should be same as your GitHub release tag version
     description = 'Human-Centered TimeSeries Analysis Package',
     author = 'Andre Sekulla',
     author_email = 'andreseku@gmail.com',
     url = 'https://github.com/AndreSeku/humancenteredTSA',
-    download_url = '',
+    download_url = 'https://github.com/AndreSeku/humancenteredTSA/archive/refs/tags/v0.0.3.tar.gz',
     keywords = ['timeseries', 'analysis', 'hcml'],
     classifiers = [],
 )
