9c2fd572b35627f24d27a6c318cd4dd36ee2831d
AndreSeku <andreseku@gmail.com>
2021-07-09 15:33:37 +0200

    typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages = ['hctsa'],
     version = 'v0.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'Human-Centered TimeSeries Analysis Package',
-    author = 'André Sekulla',
+    author = 'Andre Sekulla',
     author_email = 'andreseku@gmail.com',
     url = 'https://github.com/AndreSeku/humancenteredTSA',
     download_url = 'https://github.com/AndreSeku/humancenteredTSA/archive/refs/tags/v0.1.tar.gz',
